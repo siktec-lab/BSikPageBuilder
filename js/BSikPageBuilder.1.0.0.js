f6cf259ca1b339b0ce187fb322855e6c843ebe5e
shlomohass <shlomohassid@gmail.com>
2021-06-14 16:09:02 +0300

    Added sub controls menues and improved styling

--- js/BSikPageBuilder.1.0.0.js
+++ js/BSikPageBuilder.1.0.0.js
@@ -18,16 +18,16 @@ version:
             libraries: ["Bootstrap v.5.0.1"],
             viewTogglerClass: "builder_show",
             handlers: {
-                addContainer: function() {
+                addContainer: function(el) {
                     let _to = this.currentWorking();
                     return this.addElement("container", _to);
                 },
-                toggleView: function() {
+                toggleView: function(el) {
                     return this.toggleBuilderStyleView();
                 },
-                subControl: function(name) {
-                    console.log(name);
-                    return this.toggleBuilderStyleView();
+                subControl: function(el, sub) {
+                    //console.log(el, sub);
+                    return this.toggleSubControl(el, sub);
                 }
             }
         };
@@ -46,22 +46,33 @@ version:
             "body { padding: 20px; }",
             ".struct_ele { outline:1px solid #d1d8ff; min-height:35px; padding-bottom: 1px; margin-bottom:25px; }",
             ".struct_ele:hover {outline:1px solid blue;}",
-            ".struct_ele.active_wroking {outline:2px solid blue;}",
+            ".struct_ele.active_working {outline:2px solid blue;}",
             ".struct_ele .sik_tagging {font-size: 10px; color: #878787; vertical-align: text-top;}"
         ]
         self.workingOn = "";
         // init
         init = function() {
+            //Expend settings:
             self.settings = $.extend(true, {}, self.defaults, options);
             self.el = el;
             self.$el = $(el);
+            //Build HTML Structure:
             build();
+            //Save core elements:
             self.$frame = self.$el.find(">.builder-document").eq(0).contents();
             self.$doc = self.$frame.find("body").eq(0);
+            self.$controls = self.$el.find(">.controls>li");
+            self.$subControlsGroups = self.$el.find(">.working-controls>.sub-controls");
+            self.$subControls = self.$subControlsGroups.find(">li");
+            //Sets core styles inside the iframe:
             pushCoreStyles();
-            self.toggleBuilderStyleView("show");
+            //Set builder mode:
+            self.toggleBuilderStyleView("show"); /* SH: added - 2021-06-14 => sets builder view should be controlled through the settings */
+            //Set working on pointer:
             self.workingOn = self.$doc;
+            //Attach main and sub controls:
             attachControls();
+            //Attach events of iframe elements:
             attachDocumentEvents();
         };
 
@@ -100,6 +111,20 @@ version:
                     break;
             }
         };
+        self.toggleSubControl = function(from, which) {
+            let toShow = self.$subControlsGroups.filter("[data-name='" + which + "']");
+            //console.log(which, self.$subControlsGroups.length, toShow.length);
+            if (toShow.length) {
+                self.$subControlsGroups.not(toShow).hide();
+                self.$controls.filter("[data-run='subControl']").removeClass("selected");
+                if (toShow.is(":visible")) {
+                    toShow.hide();
+                } else {
+                    toShow.show();
+                    $(from).addClass("selected");
+                }
+            }
+        };
         //Attach Iframe styles 
         pushCoreStyles = function() {
             //Set Libraries:
@@ -130,22 +155,35 @@ version:
 
         //Attach Controls:
         attachControls = function() {
-            self.$controls = self.$el.find(">.controls>li")
-            self.$controls.each(function(i, e) {
-                $(e).on("click", function() {
+            //Main controls:
+            self.$controls.each(function() {
+                $(this).on("click", function() {
                     let handler = self.settings.handlers[$(this).data("run")];
+                    let params = $(this).clone().data("params") ?? [];
+                    console.log(params);
+                    if (typeof handler === "function") {
+                        params.unshift(this);
+                        handler.apply(self, params);
+                    }
+                });
+            });
+            //Sub controls:
+            self.$subControls.each(function() {
+                $(this).on("click", function() {
+                    let handler = self.settings.handlers[$(this).data("run")];
+                    let params = $(this).data("params") ?? [];
                     if (typeof handler === "function")
-                        handler.apply(self, []);
+                        handler.apply(self, params);
                 });
             });
         };
         attachDocumentEvents = function() {
-            //Selecting elemnts for edits:
+            //Selecting elements for edits:
             self.$doc.on("click", ".struct_ele", function(ev) {
                 ev.stopPropagation();
                 self.workingOn = $(this);
-                self.$doc.find(".struct_ele").not(self.workingOn).removeClass("active_wroking");
-                self.workingOn.toggleClass("active_wroking");
+                self.$doc.find(".struct_ele").not(self.workingOn).removeClass("active_working");
+                self.workingOn.toggleClass("active_working");
             });
         };
         // private methods
